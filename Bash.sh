441840b527a9e13fce8797915c42f9c4ef1767f8
auraecosystem <sandraisom@icloud.com>
2026-06-15 03:45:23 +0100

    Create Bash.sh

---
+++ Bash.sh
@@ -0,0 +1,11 @@
+git clone https://chromium.googlesource.com/website
+cd website
+ git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
+export PATH=/path/to/depot_tools:$PATH
+git clone https://chromium.googlesource.com/website
+$ fetch website
+ gclient sync
+$ ./npmw build
+$ git commit -a -m 'whatever'
+$ git-cl upload
+ ./npmw start
