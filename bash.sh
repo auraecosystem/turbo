a12e2ef4dfdaf0473729a363263faca4b664085d
auraecosystem <sandraisom@icloud.com>
2026-06-17 03:33:15 +0100

    Update and rename bash to bash.sh

--- bash.sh
+++ bash.sh
@@ -2,3 +2,4 @@ rpmbuild --define "optflags -O3 -march=native" -ba your_package.spec
 rpm --eval "%{optflags}"
 $ curl https://yangcatalog.org/api/search/vendors
 $ curl https://yangcatalog.org/api/search/catalog
+
