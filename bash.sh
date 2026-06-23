00f876e4b9e943a11ec1fe9e134d2b2012a015fb
Seriki Walter Yakub <sandraisom@icloud.com>
2026-06-23 11:11:01 +0100

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,3 +1,5 @@
+npm install -g elm
+brew install elm
 rpmbuild --define "optflags -O3 -march=native" -ba your_package.spec
 rpm --eval "%{optflags}"
 $ curl https://yangcatalog.org/api/search/vendors
@@ -5,3 +7,12 @@ $ curl https://yangcatalog.org/api/search/catalog
 git clone https://github.com/auraecosystem/turbo.git
 cd turbo
 tree -L 2
+npm install -g elm-live
+npm install -g create-elm-app
+create-elm-app my-app
+cd my-app
+npm start
+npx elm-live src/Main.elm --open -- --output=main.js
+mkdir my-elm-app
+cd my-elm-app
+elm init
