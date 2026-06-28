b6c9b0fd3f290edf1ccdbba922231e464b043587
Seriki Walter Yakub <sandraisom@icloud.com>
2026-06-28 12:23:10 +0100

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -21,3 +21,16 @@ cd ~/Desktop
 
 # Windows (but with <username> filled in with your user name)
 cd C:\Users\<username>\Desktop
+"git clone" https://github.com/auraecosystem/make.universe.git
+cd make.universe
+"git clone" https://github.com/auraecosystem/turbo.build.git
+cd turbo.build
+# Global install
+pnpm add turbo --global
+# Install in repository
+pnpm add turbo --save-dev --workspace-root
+
+# Global install
+bun install turbo --global
+# Install in repository
+bun install turbo --dev
